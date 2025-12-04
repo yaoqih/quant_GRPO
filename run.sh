537f205bb7b001485147c42325d1a7d7e63d9507
huyaoqi <1375626371@qq.com>
2025-12-04 11:37:11 +0800

    rl stable

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/yaoqih/quant_GRPO
 sudo apt-get install aria2 -y
 aria2c -x 16 -s 16 https://img.blenet.top/file/vast-ai-volumn/qlib_1d.zip
+aria2c -x 16 -s 16 https://img.blenet.top/file/vast-ai-volumn/pretrain.pt
+mv pretrain.pt ./quant_GRPO/data/
 # aria2c -x 16 -s 16 https://f005.backblazeb2.com/file/vast-ai-volumn/qlib_1d.zip
 unzip qlib_1d.zip -d ./quant_GRPO/data/
 # pip install blinker cryptography --ignore-installed -i https://pypi.tuna.tsinghua.edu.cn/simple
